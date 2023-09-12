62ebcbd4e0d1be40b78fa18fc60f929a7ff3b297
Mr. Brown <gitcherrypick.work@gmail.com>
2023-09-12 23:33:39 +0500

    refactor: optimization for more query methods

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -16,6 +16,37 @@ async function fetchPost(id) {
   return response.json()
 }
 
+async function createPost(postData) {
+  const response = await fetch(`${API_URL}`, {
+    method: "POST",
+    headers: { 
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postData)
+  })
+
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+
+  return response.json()
+}
+
+async function updatePost(id, postData) {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(postData)
+  })
+
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  return response.json()
+}
+
 async function deletePost(id) {
   const response = await fetch(`${API_URL}/${id}`, {
     method: "DELETE"
@@ -30,4 +61,4 @@ async function deletePost(id) {
   return response.json()
 }
 
-export { fetchAllPosts, fetchPost, deletePost }
+export { fetchAllPosts, fetchPost, deletePost, createPost, updatePost }
